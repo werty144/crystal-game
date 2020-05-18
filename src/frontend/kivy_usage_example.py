324968f193a0a657b239558519508b7cff2fe484
Evgeny Im <evgenyim6456@gmail.com>
2020-05-18 16:55:49 +0300

    test graphics

--- src/frontend/kivy_usage_example.py
+++ src/frontend/kivy_usage_example.py
@@ -1,12 +1,42 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
+from kivy.clock import Clock
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import Rectangle
+from kivy.app import App
+from kivy.graphics.svg import Svg
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatter import Scatter
 
-class MyApp(App):
+
+class MyWidget(Scatter):
+    i = 0
+
+    def __init__(self, filename, **kwargs):
+        super(MyWidget, self).__init__(**kwargs)
+        with self.canvas:
+            Rectangle(source='/home/evgeny/spdu/kivy/crystal-game/images/test.png')
+        Clock.schedule_interval(self.update, 5 / 60.)
+
+    def update(self, delta):
+        self.i += 1
+        self.canvas.clear()
+        self.canvas.add(Color(1., 1., 1.))
+        self.canvas.add(Rectangle(source='/home/evgeny/spdu/kivy/crystal-game/images/test{}.png'.format(self.i % 5)))
+        print(self.i)
+
+
+class SvgApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        self.root = FloatLayout()
+
+        svg1 = MyWidget('/home/evgeny/spdu/kivy/crystal-game/images/cloud.svg')
+        self.root.add_widget(svg1)
+        svg1.center = Window.center
 
 
-if __name__ == '__main__':
-    MyApp().run()
+if __name__ == "__main__":
+    SvgApp().run()
