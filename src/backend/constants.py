d95721b3b8106afa875d9608c030ccb09ba82f0c
Anton144 <anton-paramonov2000@yandex.ru>
2020-07-18 16:24:56 +0300

    rule widget

--- src/backend/constants.py
+++ src/backend/constants.py
@@ -8,5 +8,6 @@ LEFT = 'LEFT'
 PROJECT_PATH = dirname(dirname(dirname(abspath(__file__))))
 LEVELS_PATH = join(PROJECT_PATH, 'resources', 'levels')
 IMAGES_PATH = join(PROJECT_PATH, 'resources', 'images')
+ARROW_IMAGE = 'yan.jpg'
 
 FRAME_RATE_SEC = 0.01
