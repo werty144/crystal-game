9368a948c4a8f170bfbccc489aa3f12655debba5
Anton144 <anton-paramonov2000@yandex.ru>
2020-07-29 21:32:49 +0300

    rule widget refactor

--- src/frontend/custom_widgets/PlaygroundWidget.py
+++ src/frontend/custom_widgets/PlaygroundWidget.py
@@ -8,6 +8,7 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.widget import Widget
+from src.frontend.custom_widgets.RulesScrollViewWidget import RulesScrollViewWidget
 
 from src.backend.Engine import Engine
 from src.backend.constants import FRAME_RATE_SEC
@@ -22,7 +23,7 @@ class Playground(Widget):
     grid = ObjectProperty()
     target_field_widgets = ListProperty()
     is_target_field = BooleanProperty(False)
-    scroll_view = Property(None)
+    rules_scroll_view = Property(None)
     update_event = ObjectProperty(None, allownone=True)
     storage = ObjectProperty()
 
@@ -31,7 +32,7 @@ class Playground(Widget):
         self.add_missing_game_widgets()
         self.make_grid()
         self.set_target_field_widgets()
-        self.make_scroll_view(self.engine.get_all_rules(), lambda _: None)
+        self.make_rules_scroll_view(self.engine.get_all_rules(), lambda _: None)
         self.update_event = Clock.schedule_interval(self.update, FRAME_RATE_SEC)
 
     def update(self, _):
@@ -102,35 +103,23 @@ class Playground(Widget):
                 self.add_widget(widg)
         self.is_target_field = not self.is_target_field
 
-    def make_scroll_view(self, rules, click_on_rule_function):
-        if self.scroll_view is not None:
-            self.remove_widget(self.scroll_view)
-        pos, size = self.engine.screen_utils.get_scrollview_pos_n_size()
-        self.scroll_view = ScrollView(size_hint=(None, None), size=(size[0], size[1]), pos=(pos[0], pos[1]))
-        layout = GridLayout(cols=1, spacing=50, padding=(0, 50), size_hint_y=None)
-        layout.bind(minimum_height=layout.setter('height'))
+    def make_rules_scroll_view(self, rules, click_on_rule_function):
+        if self.rules_scroll_view is not None:
+            self.remove_widget(self.rules_scroll_view)
+        self.rules_scroll_view = RulesScrollViewWidget()
+        self.add_widget(self.rules_scroll_view)
         if len(rules) == 0:
-            # Write that there is no rules
-            self.scroll_view = ScrollView(size_hint=(None, None), size=(size[0], size[1]), pos=(pos[0], pos[1]))
-            label = Label(text='No rules', color=(0, 0, 0, 1), font_size=(0.4 * min(self.height / 4, self.width)))
-            self.scroll_view.add_widget(label)
-            with self.scroll_view.canvas.before:
-                Color(1, 1, 1, 1)
-                Rectangle(size=self.scroll_view.size, pos=self.scroll_view.pos)
-            self.add_widget(self.scroll_view)
-            return
-        max_right_side_len = max([len(rule.result_box_kinds) for rule in rules])
-        for rule in rules:
-            rule_widget = RuleWidget(rule, click_on_rule_function, max_right_side_len)
-            layout.add_widget(rule_widget)
-        self.scroll_view.add_widget(layout)
-        with self.scroll_view.canvas.before:
-            Color(1, 1, 1, 1)
-            Rectangle(size=self.scroll_view.size, pos=self.scroll_view.pos)
-        self.add_widget(self.scroll_view)
+            # Here image instead of label would be, so no need to calculate font size properly
+            label = Label(text='No rules', color=(0, 0, 0, 1), font_size=0.4 * self.rules_scroll_view.width)
+            self.rules_scroll_view.ids.grid.add_widget(label)
+        else:
+            max_right_side_len = max([len(rule.result_box_kinds) for rule in rules] + [1])
+            for rule in rules:
+                rule_widget = RuleWidget(rule, click_on_rule_function, max_right_side_len)
+                self.rules_scroll_view.ids.grid.add_widget(rule_widget)
 
     def show_all_rules(self):
-        self.make_scroll_view(self.engine.get_all_rules(), lambda rule: None)
+        self.make_rules_scroll_view(self.engine.get_all_rules(), lambda rule: None)
 
     def undo(self):
         self.engine.undo()
