38660cf666bc4cfba713347c2b32e7ae05a8a654
Evgeny Im <evgenyim6456@gmail.com>
2020-09-05 21:32:12 +0300

    normalize labels in StarsPopup

--- src/frontend/custom_widgets/StarsPopup.py
+++ src/frontend/custom_widgets/StarsPopup.py
@@ -2,4 +2,15 @@ from kivy.uix.popup import Popup
 
 
 class StarsPopup(Popup):
-    pass
+    '''Binding function that normalizes length of all labels to the last label'''
+    def on_pre_open(self):
+        self.ids.grid.children[0].ids.label.bind(final_width=self.normalize_width)
+
+    def normalize_width(self, *args):
+        self.ids.grid.children[0].ids.label.unbind(final_width=self.normalize_width)
+        max_width = 0
+        for widget in self.ids.grid.children:
+            max_width = max(max_width, widget.ids.label.width)
+        for widget in self.ids.grid.children:
+            widget.ids.label.width = max_width
+        self.ids.grid.children[0].ids.label.bind(final_width=self.normalize_width)
