5ab30372a093fd7bfbeee69b7ff00a1fb37ff650
Anton144 <anton-paramonov2000@yandex.ru>
2020-11-29 22:14:44 +0300

    level numbers + qiwi correct link

--- src/frontend/Screens/LevelsScreen.py
+++ src/frontend/Screens/LevelsScreen.py
@@ -2,6 +2,8 @@ import re
 
 from kivy.properties import ObjectProperty, NumericProperty
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 
 from src.backend.constants import ID_TEXTURE_MAP, MODULE_OFFSET, LEVELS_PER_MODULE
@@ -18,17 +20,31 @@ class LevelsScreen(Screen):
         for i in range(len(btn_list)):
             grid_layout.remove_widget(btn_list[-1])
         for i in range(1, LEVELS_PER_MODULE[self.module] + 1):
-            grid_layout.add_widget(
+            btn_with_txt = FloatLayout()
+            btn_with_txt.add_widget(
                 Button(
-                    text="Level {0}".format(i + MODULE_OFFSET[self.module]),
                     background_normal=self.get_button_image(i)[0],
                     background_down=self.get_button_image(i)[1],
                     on_press=lambda _, lvl=i: self.go_to_lvl(lvl),
-                    border=(0, 0, 0, 0)
+                    border=(0, 0, 0, 0),
+                    pos_hint={'x': 0, 'y': 0}
                 )
             )
+            btn_with_txt.add_widget(
+                Label(
+                    text=f'[color=000000][b]{i + MODULE_OFFSET[self.module]}[/b][/color]',
+                    font_size=20,
+                    size_hint=(0.3, 0.3),
+                    pos_hint={'right': 0.95, 'y': 0},
+                    markup=True,
+                    halign='right'
+                )
+            )
+            grid_layout.add_widget(
+                btn_with_txt
+            )
         for obj in btn_list:
-            if type(obj) is Button:
+            if isinstance(obj, Button):
                 lvl = int(re.search(r'\d+', obj.text).group())
                 button_image = self.get_button_image(lvl)
                 obj.background_normal = button_image[0]
